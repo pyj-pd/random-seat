c0041775386fe607cf259b9ed6c80dd1a943d3b7
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2024-07-20 12:10:26 +0900

    cleanup main.ts file

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import { routes } from './router'
-import { createHead } from '@unhead/vue'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-export const createApp = ViteSSG(
-  App,
-  { routes },
-  ({ app, router, routes, isClient, initialState }) => {
-    // Pinia
-    const pinia = createPinia()
-    pinia.use(piniaPluginPersistedstate)
+export const createApp = ViteSSG(App, { routes }, ({ app }) => {
+  // Pinia
+  const pinia = createPinia()
+  pinia.use(piniaPluginPersistedstate)
 
-    app.use(pinia)
-  },
-)
+  app.use(pinia)
+})
