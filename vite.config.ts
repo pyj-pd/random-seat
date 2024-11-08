5e03044d096ee1e91fd264f54542823cf32a18ac
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2024-11-09 03:10:03 +0900

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     outDir: 'out',
     emptyOutDir: true,
   },
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
