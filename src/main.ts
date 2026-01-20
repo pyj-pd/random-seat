efc62b8d495a3a81fee84180cf6cded2a91fb08d
pyj-pd <123401626+pyj-pd@users.noreply.github.com>
2026-01-20 14:18:13 +0900

    fix SSG. change license.

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,11 @@ import App from './App.vue'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import { ViteSSG } from 'vite-ssg'
-import { createWebHistory } from 'vue-router'
 
-export const createApp = ViteSSG(
-  App,
-  { routes, history: createWebHistory(import.meta.env.BASE_URL) },
-  ({ app }) => {
-    // Pinia
-    const pinia = createPinia()
-    pinia.use(piniaPluginPersistedstate)
+export const createApp = ViteSSG(App, { routes, base: import.meta.env.BASE_URL }, ({ app }) => {
+  // Pinia
+  const pinia = createPinia()
+  if (!import.meta.env.SSR) pinia.use(piniaPluginPersistedstate)
 
-    app.use(pinia)
-  },
-)
+  app.use(pinia)
+})
