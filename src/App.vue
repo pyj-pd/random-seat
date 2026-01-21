ffbdf79a7c09b042e55f9c4176f00b007c382d28
pyj-pd <123401626+pyj-pd@users.noreply.github.com>
2026-01-21 11:52:51 +0900

    change favicon sizes. add json-ld data.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <script setup lang="ts">
 import { useSeoMeta, useHead } from '@unhead/vue'
 import { defaultMetadata } from './router'
-import { SITE_NAME, SITE_URL } from './constants/project'
+import { PARENT_SITE_NAME, SITE_URL } from './constants/project'
 
 const { title, description, visibleToSearchEngines } = defaultMetadata
 
 useSeoMeta({
   title,
-  ogTitle: title,
   description,
+
+  ogTitle: title,
   ogDescription: description,
-  ogSiteName: SITE_NAME,
+  ogSiteName: PARENT_SITE_NAME,
   ogUrl: SITE_URL,
+  ogImage: `${SITE_URL}apple-icon.png`,
+
   robots: visibleToSearchEngines ? 'all' : 'none',
-  ogImage: `${SITE_URL}/apple-icon.png`,
 })
+
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  name: PARENT_SITE_NAME,
+  url: SITE_URL,
+}
 
 useHead({
   link: [
     {
       rel: 'canonical',
       href: SITE_URL,
+    },
+    {
+      rel: 'icon',
+      href: `${import.meta.env.BASE_URL}favicon.ico`,
+      type: 'image/x-icon',
+      sizes: 'any',
+    },
+    {
+      rel: 'icon',
+      href: `${import.meta.env.BASE_URL}icon.svg`,
+      type: 'image/svg+xml',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: `${import.meta.env.BASE_URL}apple-icon.png`,
+      type: 'image/png',
+      sizes: '180x180',
+    },
+  ],
+  script: [
+    {
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify(jsonLd),
     },
   ],
 })
--- src/constants/project.ts
+++ src/constants/project.ts
@@ -2,6 +2,8 @@
 
 export const GITHUB_REPO_URL = 'https://github.com/pyj-pd/random-seat'
 
+export const PARENT_SITE_NAME = 'pyj-pd'
+
 export const SITE_NAME = '무작위 자리 뽑기',
   SITE_SLOGAN = '깔끔하고 편리하게 자리 배치',
   SITE_URL = 'https://pyj-pd.github.io/random-seat/'
