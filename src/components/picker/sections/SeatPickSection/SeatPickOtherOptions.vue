b3354d0d67776af9f9b5bb53e97a69b06d932ff6
pyj-pd <123401626+pyj-pd@users.noreply.github.com>
2026-03-04 00:02:17 +0900

    move seat number option to random seat section.

--- src/components/picker/sections/SeatPickSection/SeatPickOtherOptions.vue
+++ src/components/picker/sections/SeatPickSection/SeatPickOtherOptions.vue
@@ -7,7 +7,18 @@ const { showSeatNumbers } = storeToRefs(useOptionStore())
 </script>
 
 <template>
-  <div>
+  <div :class="$style.container">
     <CheckboxInput v-model="showSeatNumbers">이름 옆에 번호 표시</CheckboxInput>
   </div>
 </template>
+
+<style lang="scss" module>
+@use '@/styles/value' as value;
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: value.$button-container-small-gap;
+}
+</style>
