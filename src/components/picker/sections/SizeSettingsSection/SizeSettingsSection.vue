bd626c336b40daf87772a6ed6c266d41b1ff6c7e
pyj-pd <123401626+pyj-pd@users.noreply.github.com>
2026-03-07 21:44:31 +0900

    add picking button on size settings section

--- src/components/picker/sections/SizeSettingsSection/SizeSettingsSection.vue
+++ src/components/picker/sections/SizeSettingsSection/SizeSettingsSection.vue
@@ -6,11 +6,12 @@ import ButtonContainer from '@/components/common/ButtonContainer.vue'
 import SectionTitle from '@/components/picker/SectionTitle.vue'
 import { useSectionNavigation } from '@/composables/useSectionNavigation'
 import { sections, type SectionId } from '@/constants/section'
-import { ArrowRight, Trash2 } from 'lucide-vue-next'
+import { ArrowRight, Settings, Trash2 } from 'lucide-vue-next'
 import TableLayout from './TableLayout.vue'
 import SeatSizeInput from './SeatSizeInput.vue'
 
 const NEXT_SECTION: SectionId = 'name-settings'
+const PICKER_SECTION_ID: SectionId = 'random-pick-seat'
 
 const { setCurrentSectionId } = useSectionNavigation()
 
@@ -27,8 +28,12 @@ const tableLayoutRef = useTemplateRef('table-layout')
         <Trash2 />
         자리 배치 초기화</ShadowButton
       >
-      <ShadowButton @click="() => setCurrentSectionId(NEXT_SECTION)"
-        >{{ sections[NEXT_SECTION].title }}으로
+      <ShadowButton @click="() => setCurrentSectionId(NEXT_SECTION)">
+        <Settings />
+        {{ sections[NEXT_SECTION].title }}
+      </ShadowButton>
+      <ShadowButton @click="() => setCurrentSectionId(PICKER_SECTION_ID)">
+        {{ sections[PICKER_SECTION_ID].title }}으로
         <ArrowRight />
       </ShadowButton>
     </ButtonContainer>
